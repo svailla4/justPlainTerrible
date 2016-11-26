a207c4dbeaee410a6b74e13cce6f65ceabbb20af
Stephane Vaillancourt <svailla4@uwo.ca>
2016-11-26 14:32:26 -0500

    made some changes

---
+++ myscript.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var searchUrl = 'http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC&limit=15';
+
+  $.get(searchUrl, (result)=>{
+    var image = result.data[0].images.downsized.url;
+
+    //$('img').each(()=>{
+    $('*').each(function(){
+
+        if ($(this).is('img')) {
+            $(this).attr('src',image);
+        }
+
+    });
+
+    //});
+
+
+    //$('img').attr('src', img);
+  });
+
+
+});
