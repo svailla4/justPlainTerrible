cf84608477262df3b55234f915c44f7b3b5fa09a
James Lee <wjjameslee@gmail.com>
2016-11-26 16:13:44 -0500

    Update
    Imgur

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
 
-  var searchUrl = 'http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC&limit=15';
+  var searchUrl = 'http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC&limit=100';
 
   $.get(searchUrl, (result)=>{
     var image = result.data[0].images.downsized.url;
