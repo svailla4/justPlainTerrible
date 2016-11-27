07ec55a0ae3cea2ef6e752f91404cb21161847b2
James Lee <wjjameslee@gmail.com>
2016-11-26 20:14:01 -0500

    Delete update.js

--- update.js
+++
@@ -1,7 +0,0 @@
-var sendMsg = ()=>{
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    alert(response.farewell);
-  });
-});
-};
